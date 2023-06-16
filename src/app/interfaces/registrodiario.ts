90846559f7dfead423c7657ac3cee1b12c3d2c40
phaetton <31552767+phaetton@users.noreply.github.com>
2023-06-16 09:19:39 -0600

    Create registrodiario.ts

---
+++ src/app/interfaces/registrodiario.ts
@@ -0,0 +1,25 @@
+export interface Registrodiario {
+    id?: string;
+    centimo1: number;
+    centimo2: number;
+    centimo5: number;
+    centimo10: number;
+    centimo20: number;
+    centimo50: number;
+    euro1: number;
+    euro2: number;
+    euro5: number;
+    euro10: number;
+    euro20: number;
+    euro50: number;
+    euro100: number;
+    papel: number;
+    bari: number;
+    bare: number;
+    barpre: number;
+    barco: number;
+    jefei: number;
+    jefee: number;
+    compra: number;
+    comentario: string;
+}
