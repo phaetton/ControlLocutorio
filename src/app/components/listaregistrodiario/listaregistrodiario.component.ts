fbd77df4361208d7f6578f8c45df148ea7d3d532
phaetton <31552767+phaetton@users.noreply.github.com>
2023-06-18 03:37:14 -0600

    Vista de gastos de barberia

--- src/app/components/listaregistrodiario/listaregistrodiario.component.ts
+++ src/app/components/listaregistrodiario/listaregistrodiario.component.ts
@@ -9,13 +9,18 @@ import { RegistrodiarioService } from 'src/app/services/registrodiario.service';
 })
 export class ListaregistrodiarioComponent {
   registros: Registrodiario[] = [];
+  debe: number = 0;
+  cobro: number = 0;
   constructor(private registroSvc: RegistrodiarioService) { }
 
   ngOnInit() {
     this.registroSvc.getRegistroDiario().subscribe(registros => {
       this.registros = registros;
+
+      this.debe = registros.reduce((prev, curr) => Number(prev) + (curr.bare ? curr.bare : 0), 0);
+      this.cobro = registros.reduce((prev, curr) => Number(prev) + (curr.barco ? curr.barco : 0), 0);
     })
-  } 
+  }
 
   async onClickDelete(registro: Registrodiario) {
     const response = await this.registroSvc.deleteRegistroDiario(registro);
