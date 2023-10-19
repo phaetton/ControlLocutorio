96312fea554e614a0d62fbc762aaf3e71c61f6ae
Rafael Francisco Serrano Urtecho <31552767+phaetton@users.noreply.github.com>
2023-10-19 11:40:32 -0700

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegistrarcategoriaComponent } from './components/registrarcategoria/registrarcategoria.component';
+import { RegistrarproductoComponent } from './components/registrarproducto/registrarproducto.component';
+import { RegistrarsubcategoriaComponent } from './components/registrarsubcategoria/registrarsubcategoria.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:DashboardComponent},
+  {path:"agregarcategoria",component:RegistrarcategoriaComponent},
+  {path:"agregarproducto",component:RegistrarproductoComponent},
+  {path:"agregarsubcategoria",component:RegistrarsubcategoriaComponent},
+  {path:"**",component:DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
