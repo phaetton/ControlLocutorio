e900ad44994aab53d777291af1ed882021f24c48
Rafael Francisco Serrano Urtecho <31552767+phaetton@users.noreply.github.com>
2023-10-19 11:40:34 -0700

    Create listarcompra.component.ts

---
+++ src/app/components/listarcompra/listarcompra.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listarcompra',
+  templateUrl: './listarcompra.component.html',
+  styleUrls: ['./listarcompra.component.scss']
+})
+export class ListarcompraComponent {
+
+}
