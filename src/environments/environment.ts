639012722770f0756a123b93f8a6443252a8dd15
Rafael Francisco Serrano Urtecho <31552767+phaetton@users.noreply.github.com>
2023-10-29 05:35:23 -0700

    reparando environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'locutorio-f8203',
+    appId: '1:988382514081:web:c52ca047426a3a8bed59e0',
+    storageBucket: 'locutorio-f8203.appspot.com',
+    apiKey: 'AIzaSyDC8MbJy4MOzUUGnmEolmYyyZV7iqbHH18',
+    authDomain: 'locutorio-f8203.firebaseapp.com',
+    messagingSenderId: '988382514081',
+    measurementId: 'G-C7MGEBX1LC',
+  },};
