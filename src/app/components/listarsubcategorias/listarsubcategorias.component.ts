977ad9f158ecdf2351584586c7364114117df187
Rafael Francisco Serrano Urtecho <31552767+phaetton@users.noreply.github.com>
2023-10-31 03:22:25 -0700

    Create listarsubcategorias.component.ts

---
+++ src/app/components/listarsubcategorias/listarsubcategorias.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listarsubcategorias',
+  templateUrl: './listarsubcategorias.component.html',
+  styleUrls: ['./listarsubcategorias.component.scss']
+})
+export class ListarsubcategoriasComponent {
+
+}
