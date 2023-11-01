da48db71a43a24425304ddc4a1b1c70aec06dd33
Rafael Francisco Serrano Urtecho <31552767+phaetton@users.noreply.github.com>
2023-11-01 05:10:44 -0700

    Update iconos.service.ts

--- src/app/services/iconos.service.ts
+++ src/app/services/iconos.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Iconos } from '../interfaces/iconos';
-import { Firestore, collection, addDoc, collectionData, doc, deleteDoc, updateDoc } from '@angular/fire/firestore';
-import { Observable, catchError, filter, map, pipe, throwError } from 'rxjs';
+import { Firestore, collection, addDoc, collectionData, doc, deleteDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -20,15 +20,9 @@ export class IconosService {
     return collectionData(iconoRef, { idField: 'id' }) as Observable<Iconos[]>;
   }
 
-
-
   deleteIconos(icono: Iconos) {
     const iconoRef = doc(this.firestore, `Iconos/${icono.id}`);
     return deleteDoc(iconoRef);
   }
 
-  updateIconos(icono: Iconos) {
-    const iconoRef = doc(this.firestore, `Iconos/LM3mlkwoxabQh1ssyHzh`);
-    return updateDoc(iconoRef, { icono });
-  }
 }
