d4fda29d1d97a60d04df0672b62f192bd59988fa
Rafael Francisco Serrano Urtecho <31552767+phaetton@users.noreply.github.com>
2023-11-01 09:23:17 -0700

    Create listarcategorias.component.ts

---
+++ src/app/components/listarcategorias/listarcategorias.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listarcategorias',
+  templateUrl: './listarcategorias.component.html',
+  styleUrls: ['./listarcategorias.component.scss']
+})
+export class ListarcategoriasComponent {
+
+}
