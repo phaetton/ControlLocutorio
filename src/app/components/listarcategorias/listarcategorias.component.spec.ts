e4657ada00f5298ca1d24dc7dfcb2451137f7712
Rafael Francisco Serrano Urtecho <31552767+phaetton@users.noreply.github.com>
2023-11-01 09:23:20 -0700

    Create listarcategorias.component.spec.ts

---
+++ src/app/components/listarcategorias/listarcategorias.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListarcategoriasComponent } from './listarcategorias.component';
+
+describe('ListarcategoriasComponent', () => {
+  let component: ListarcategoriasComponent;
+  let fixture: ComponentFixture<ListarcategoriasComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListarcategoriasComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ListarcategoriasComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
