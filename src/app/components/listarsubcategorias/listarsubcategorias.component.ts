782d7490ac9c4a94d1eb9ba3509081cf7b50845f
Rafael Francisco Serrano Urtecho <31552767+phaetton@users.noreply.github.com>
2023-11-04 01:55:45 -0700

    Update listarsubcategorias.component.ts

--- src/app/components/listarsubcategorias/listarsubcategorias.component.ts
+++ src/app/components/listarsubcategorias/listarsubcategorias.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { combineLatest } from 'rxjs';
+import { Subcategorias } from 'src/app/interfaces/subcategorias';
+import { IconosService } from 'src/app/services/iconos.service';
+import { SubcategoriasService } from 'src/app/services/subcategorias.service';
 
 @Component({
   selector: 'app-listarsubcategorias',
@@ -7,4 +11,35 @@ import { Component } from '@angular/core';
 })
 export class ListarsubcategoriasComponent {
 
+  @Input() editar: boolean = false;
+  @Output() seleccionado = new EventEmitter<string>;
+  subcategorias: Subcategorias[] = [];
+  subcategoriaselect: string = "";
+
+  constructor(private subcategoriasvc: SubcategoriasService, private iconosvc: IconosService) { }
+
+  ngOnInit() {
+
+    combineLatest(
+      [this.subcategoriasvc.getSubcategorias(),
+      this.iconosvc.getIconos()]
+    ).subscribe(([subcategorias, iconos]) => {
+      this.subcategorias = subcategorias.map(m => {
+        let valor = iconos.find(x => x.id == m.icono);
+        return {
+          id: m.id,
+          nombre: m.nombre,
+          icono: valor ? valor['img'] : ''
+        }
+      })
+    })
+  }
+
+  async onClickDelete(registro: Subcategorias) {
+    const response = await this.subcategoriasvc.deleteSubcategorias(registro);
+  }
+
+  seleccionar(idsubcategoria: string) {
+    this.seleccionado.emit(idsubcategoria);
+  }
 }
